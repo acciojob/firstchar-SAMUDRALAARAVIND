788e3a61ce4cb8898d17e637473b126d801fe132
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2023-12-13 14:46:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function firstChar(text) {
-  // your code here
+  let trimmedText = text.trim();
+  return trimmedText.charAt(0);
 }
-
 // Do not change the code below
 
 const text = prompt("Enter text:");
